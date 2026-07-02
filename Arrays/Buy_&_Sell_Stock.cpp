8a6ed081276f5bc3745b1b46c3a6b3761ffed185
Tanvi Gaikwad <tanvidgaikwad2002@gmail.com>
2026-07-02 23:58:56 +0530

    Add files via upload

---
+++ Arrays/Buy_&_Sell_Stock.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution 
+{
+	public:
+	    int maxProfit(vector<int>& prices) 
+	    {
+		int maxProfit = 0;
+		int bestBuy = prices[0];
+
+		for (int i = 1; i < prices.size(); i++) 
+		{
+		    if (prices[i] > bestBuy) 
+		    {
+		        maxProfit = max(maxProfit, prices[i] - bestBuy);
+		    }
+
+		    bestBuy = min(bestBuy, prices[i]);
+		}
+
+		return maxProfit;
+	    }
+};
+
+int main() 
+{
+    Solution obj;
+
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+
+    cout << "Maximum Profit: " << obj.maxProfit(prices) << endl;
+
+    return 0;
+}
